7459e483ad6282d2106fc236458ce6a6593d3cec
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2012-09-13 09:31:38 +0200

    Création d'une page basique

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+echo "<h1>Mon premier script PHP</h1>";
+echo "<p>Une deuxi&egrave;me ligne</p>";
+?>
+
+</body>
+</html>
